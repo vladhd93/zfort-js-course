e4d1f2ee828b0f754d5bba1a0f9624f66865761b
vladhd93 <vladhd@ukr.net>
2016-07-17 20:22:06 +0300

    done first home task

--- homework/lesson.1-2/main.js
+++ homework/lesson.1-2/main.js
@@ -5,7 +5,18 @@ json // JSON data is assigned to `json` global variable which is declared in `js
  *
  *    Produce a new array of users including fixed balance values
  */
-var users = 'Fix me';
+var parsedJSON = JSON.parse(json);
+
+var users = parsedJSON.map(function (singleUser) {
+    if (singleUser.balance == false) {
+        singleUser.balance = 0;
+    } else {
+        singleUser.balance = Math.round(parseFloat(singleUser.balance));
+    }
+    return singleUser;
+});
+
+//users = JSON.stringify(users);
 
 
 /*
@@ -16,7 +27,23 @@ var users = 'Fix me';
  *    and value is array of users
  *    { '1000-2000': [], '2000+': [] }
  */
-var grouped = 'Fix me';
+
+var grouped = {
+    '1000-2000': [],
+    '2000+': []
+};
+
+users.map(function (singleUser) {
+    if (singleUser.balance >= 1000 && singleUser.balance <= 2000) {
+        grouped['1000-2000'].push(singleUser);
+
+    } else if (singleUser.balance > 2000) {
+        grouped['2000+'].push(singleUser);
+    }
+});
+
+//grouped = JSON.stringify(grouped);
+console.log(grouped);
 
 
 /*
@@ -28,4 +55,41 @@ var grouped = 'Fix me';
  *    average age and average balance
  *    { '1000-2000': { avgAge: Number, avgBalance: Number }, ... }
  */
-var averages = 'Fix me';
+var averages = {
+    '1000-2000': {
+        avgAge: null,
+        avgBalance: null
+    },
+    '2000+': {
+        avgAge: null,
+        avgBalance: null
+    }
+};
+
+for (var i = 0; i < grouped['1000-2000'].length; i++) {
+    averages['1000-2000'].avgAge += grouped['1000-2000'][i].age;
+    averages['1000-2000'].avgBalance += grouped['1000-2000'][i].balance;
+}
+
+
+averages['1000-2000'].avgAge = averages['1000-2000'].avgAge / grouped['1000-2000'].length;
+averages['1000-2000'].avgBalance = averages['1000-2000'].avgBalance / grouped['1000-2000'].length;
+console.log(averages['1000-2000'].avgAge);
+console.log(averages['1000-2000'].avgBalance);
+
+
+for (var k = 0; k < grouped['2000+'].length; k++) {
+    averages['2000+'].avgAge += grouped['2000+'][k].age;
+    averages['2000+'].avgBalance += grouped['2000+'][k].balance;
+}
+
+averages['2000+'].avgAge = averages['2000+'].avgAge / grouped['2000+'].length;
+averages['2000+'].avgBalance = averages['2000+'].avgBalance / grouped['2000+'].length;
+console.log(averages['2000+'].avgAge);
+console.log(averages['2000+'].avgBalance);
+
+
+
+
+
+
