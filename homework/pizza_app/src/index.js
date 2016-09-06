e8f71ef01e96f46ea17f5f59c4f1d8bd12f601c8
vladhd93 <vladhd@ukr.net>
2016-09-07 01:25:33 +0300

    add pizzas slider

--- homework/pizza_app/src/index.js
+++ homework/pizza_app/src/index.js
@@ -1,5 +1,6 @@
-function PizzaBuilder(){
+function PizzaBuilder(pizzaList){
     this.data = null;
+    this.pizzaList = document.querySelector(pizzaList);
 }
 
 PizzaBuilder.prototype.renderSidebar = function(list,data){
@@ -25,7 +26,7 @@ PizzaBuilder.prototype.renderSidebar = function(list,data){
     xhr.send();
 };
 
-function Table(addBtn, basket, itemCount,numBtn,ingrList,totalCell) {
+function Table(addBtn, basket, itemCount,numBtn,ingrList,totalCell,pizzaList,pizzaListItem) {
     this.addBtn = document.querySelector(addBtn);
     this.basket = document.querySelector(basket);
     this.itemCount = document.querySelector(itemCount);
@@ -33,10 +34,11 @@ function Table(addBtn, basket, itemCount,numBtn,ingrList,totalCell) {
     this.ingrList = document.querySelector(ingrList);
     this.totalCell = document.querySelector(totalCell);
     this.currentIndex = 2;
+    this.pizzaList = document.querySelector(pizzaList);
+    this.pizzaListItem = document.querySelector(pizzaListItem);
 }
 
 Table.prototype.renderRow = function () {
-
     var _self = this;
     function renderRowHandler() {
         return function () {
@@ -50,33 +52,50 @@ Table.prototype.renderRow = function () {
                                <button class="remove">x</button>
                            </td>
                            </tr>`;
+            var pizzaTemplate = `
+                <li class="pizza-item">
+                    <span class="pizza-index">${_self.currentIndex}</span>
+                    <img src="images/pizza.svg" width="300" height="300" alt="" title="">
+                </li>
+            `;
+            _self.pizzaList.innerHTML +=  pizzaTemplate;
             _self.basket.innerHTML += rowTemplate;
             _self.currentIndex += 1;
             _self.checkCurrentIndex();
             _self.updateItemCount();
             _self.setCurrentItem();
+            _self.sumPrice('.total','.sum-price');
         };
     }
     _self.addBtn.addEventListener("click", renderRowHandler());
-
 };
 
-Table.prototype.removeItem = function (event) {
+Table.prototype.removeItem = function () {
     var _self = this;
     function removeSelfHandler(event) {
         if (event.target.className == 'remove') {
-            var removeRow = event.target.parentNode.parentNode;
+            var removeRow = event.target.parentNode.parentNode,
+             nodeList = Array.prototype.slice.call(_self.basket.children),
+             currentIndex = nodeList.indexOf( removeRow );
+            _self.pizzaList.removeChild(document.querySelectorAll('.list li')[currentIndex]);
             _self.basket.removeChild(removeRow);
             _self.checkCurrentIndex();
+            _self.updateItemCount();
             _self.setCurrentItem();
+            _self.sumPrice('.total','.sum-price');
         }
     }
     document.querySelector('body').addEventListener('click', removeSelfHandler);
 };
 
 Table.prototype.setCurrentItem = function () {
-    var btns = [].slice.call(document.querySelectorAll('.select-num'));
-    function clickHandler(){
+    var _self = this,
+        btns = [].slice.call(document.querySelectorAll('.select-num'));
+    function clickHandler(event){
+        var currentRow = event.target.parentNode.parentNode,
+            nodeList = Array.prototype.slice.call(_self.basket.children),
+            currentIndex = nodeList.indexOf( currentRow );
+        document.querySelector('.list').style.marginLeft = -`${300*currentIndex}`+`px`;
         var tr = [].slice.call(document.querySelectorAll('.basket tr'));
         for(var i = 0; i < tr.length;i++){
             tr[i].classList.remove('selected');
@@ -88,6 +107,17 @@ Table.prototype.setCurrentItem = function () {
     }
 };
 
+Table.prototype.sumPrice = function(sumArr,cell){
+    var arr = [].slice.call(document.querySelectorAll(sumArr)),
+        sum = arr.reduce(function(sum,currentItem){
+        return sum + (+currentItem.dataset.price);
+    },0);
+    console.log(sum);
+    var displaySumCell = document.querySelector(cell);
+    displaySumCell.dataset.price = sum;
+    displaySumCell.innerHTML = sum;
+};
+
 Table.prototype.updateItemCost = function () {
     var _self= this;
     function listHandler(event){
@@ -96,30 +126,27 @@ Table.prototype.updateItemCost = function () {
         if (!_self.ingrList.contains(li)){
             return;
         }
-        var cost = parseInt(target.parentNode.dataset.price);
-        var currentTotalCost = document.querySelector(".basket .selected .total");
+        var cost = parseInt(target.parentNode.dataset.price),
+            currentTotalCost = document.querySelector(".basket .selected .total");
         if(currentTotalCost){
             currentTotalCost.dataset.price = parseInt(currentTotalCost.dataset.price) + cost;
             currentTotalCost.innerHTML = currentTotalCost.dataset.price;
+            _self.sumPrice('.total','.sum-price');
         }
     }
     this.ingrList.addEventListener('click',listHandler);
 };
 
 Table.prototype.checkCurrentIndex = function () {
-    var itemsArray = [].slice.call(document.querySelectorAll(".orders tr .select-num"));
-    var i;
+    var itemsArray = [].slice.call(document.querySelectorAll(".orders tr .select-num")),
+        pizzasArray = [].slice.call(document.querySelectorAll(".pizza-index")),
+        i,
+        k;
     for (i = 0; i < itemsArray.length; i++) {
         itemsArray[i].innerHTML = i + 1;
     }
-    var checkItemArray = document.querySelectorAll('.orders tr');
-    if (checkItemArray.length == 2) {
-        this.currentIndex = 3;
-        i = 2;
-    }
-    if (checkItemArray.length == 1) {
-        this.currentIndex = 2;
-        i = 1;
+    for (k = 0; k < pizzasArray.length; k++) {
+        pizzasArray[k].innerHTML = k + 1;
     }
 };
 
@@ -128,13 +155,14 @@ Table.prototype.updateItemCount = function () {
     this.itemCount.innerHTML = itemsArray.length;
 };
 
-var table = new Table('.add-pizza', '.basket tbody', '.num','.select-num','.ingredients-list','.total');
+var table = new Table('.add-pizza', '.basket tbody', '.num','.select-num','.ingredients-list','.total','.list','.pizza-item');
 table.checkCurrentIndex();
 table.renderRow();
 table.removeItem();
 table.updateItemCount();
 table.updateItemCost();
 table.setCurrentItem();
+table.sumPrice('.total','.sum-price');
 var pizzaBuilder = new PizzaBuilder();
 pizzaBuilder.renderSidebar('.ingredients-list','public/config.json');
 
